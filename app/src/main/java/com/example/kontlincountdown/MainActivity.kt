3f89d5b7eb1314ea79dd1cc0fac326fd478fd6f5
Antalyaspor <kaya_umit_07@msn.com>
2023-03-05 23:43:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kontlincountdown/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.kontlincountdown
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        object :CountDownTimer(10000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+               textView.text="Left: ${(millisUntilFinished/1000).toString()}"
+            }
+
+            override fun onFinish() {
+                textView.text="Finish"
+            }
+
+        }.start()
+
+
+    }
+}
